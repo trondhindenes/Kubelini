c8c24bfbe92ec030cd7dd3f6947a52746ac4a309
Trond Hindenes <trond@hindenes.com>
2018-09-16 20:08:00 +0200

    working commit for apiserver-in-pod

--- roles/kubelini-worker-kubelet/files/generate_node_kubeconfig.sh
+++ roles/kubelini-worker-kubelet/files/generate_node_kubeconfig.sh
@@ -3,22 +3,23 @@ set -e
 
 
 KUBERNETES_PUBLIC_ADDRESS=$1
+TARGET_KUBECONFIG_FILE=$2
 
 kubectl config set-cluster kubelini \
     --certificate-authority=/opt/kubelini/ca/ca.pem \
     --embed-certs=true \
     --server=https://${KUBERNETES_PUBLIC_ADDRESS}:6443 \
-    --kubeconfig=/opt/kubelini/${HOSTNAME}.kubeconfig
+    --kubeconfig=${TARGET_KUBECONFIG_FILE}
 
   kubectl config set-credentials system:node:${HOSTNAME} \
     --client-certificate=/opt/kubelini/pki/${HOSTNAME}.pem \
     --client-key=/opt/kubelini/pki/${HOSTNAME}-key.pem \
     --embed-certs=true \
-    --kubeconfig=/opt/kubelini/${HOSTNAME}.kubeconfig
+    --kubeconfig=${TARGET_KUBECONFIG_FILE}
 
   kubectl config set-context default \
     --cluster=kubelini \
     --user=system:node:${HOSTNAME} \
-    --kubeconfig=/opt/kubelini/${HOSTNAME}.kubeconfig
+    --kubeconfig=${TARGET_KUBECONFIG_FILE}
 
-  kubectl config use-context default --kubeconfig=/opt/kubelini/${HOSTNAME}.kubeconfig
+  kubectl config use-context default --kubeconfig=${TARGET_KUBECONFIG_FILE}
